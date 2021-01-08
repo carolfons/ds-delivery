b86fdc83648cf0b2a1247bb237b3e9f02bf91810
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-01-08 01:09:41 -0300

    Get/products

---
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsdelivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsdelivery.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+	
+}
