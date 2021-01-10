b00c712626405bdf5c626d1785339740966d43e2
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-01-10 20:26:28 -0300

    addin navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './NavBar';
 
 function App() {
   return (
     <div>
-     <h1> Hello World </h1>
-     
+     <Navbar/>
     </div>
   );
 }
---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import {ReactComponent as Logo} from "./logo.svg";
+
+function Navbar(){
+  return(
+    <nav className = "main-navbar" >
+      <Logo/>
+    <a href = "home" className = "logo-text">DS Delivery</a>
+
+    </nav>
+  )
+}
+
+export default Navbar;
