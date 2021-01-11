ff5e5ecc9f8d7916b7469b213a4fd92af7188078
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-01-11 00:53:58 -0300

    Front-end web part 1 done

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders(){
+
+  const[products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts().then(response => setProducts(response.data)).catch(error=>console.log(error));
+
+  }, []);
+
   return(
     <div className="orders-container">
       <StepsHeader/>
+      <ProductsList products = {products}/>
     </div>
   );
 }
