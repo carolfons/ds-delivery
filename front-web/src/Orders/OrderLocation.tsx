a9cb04855516343176d9cb2a62dd31ca81cb866e
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-01-11 04:16:23 -0300

    Front web done

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {MapContainer, TileLayer, Marker,Popup} from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
--- front-web/src/Orders/OrderSummary.tsx
+++ front-web/src/Orders/OrderSummary.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { formatPrice } from './helpers';
 
 type Props = {
