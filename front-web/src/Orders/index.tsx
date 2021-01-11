8b9093ffe8d2ced5b0cde9512b44e25d55aa334c
Caroline S Fonseca <carolinefonseca@gec.inatel.br>
2021-01-10 22:46:11 -0300

    Add StepsHeader

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles.css';
+import StepsHeader from './StepsHeader';
 
 function Orders(){
   return(
-    <div>
-      <h1>Orders</h1>
+    <div className="orders-container">
+      <StepsHeader/>
     </div>
   );
 }
